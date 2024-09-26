dae964fe5326317b7f235f930324d416f45eec3f
JingLian-pvt <ngjinglian@hotmail.com>
2024-09-26 18:41:41 -0500

    Modified module list for 'distortion'

--- .history/distortion_20240926183406.py
+++ .history/distortion_20240926183406.py
@@ -3,13 +3,24 @@
 """
 Created on Mon Aug 19 16:29:58 2024
 
-generate an amorphous system based on shake N break
-
 @author: jinglian
 """
 
+from pymatgen.core.surface import SlabGenerator, generate_all_slabs
 from pymatgen.io.vasp import Poscar
+from pymatgen.io.cif import CifWriter
+from pymatgen.transformations.standard_transformations import PrimitiveCellTransformation
+import os
+import sys
+import ase
+import numpy as np
+import pymatgen
+import doped
+from importlib_metadata import version
+
 import shakenbreak.distortions
+import shakenbreak.input
+from pymatgen.core.structure import Structure
 
 poscar = Poscar.from_file("/home/jinglian/Documents/crystal-structure/NiFeOOH/vcrelax/Uniform/distort.cube.vasp")
 pmg_struct = poscar.structure
